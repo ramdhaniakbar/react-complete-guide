4d6421fa6ad27c73975af58a8879e4570ff5fe3d
Muhamad Ramdhani Akbar <ramdhaniakbarmuhamad@gmail.com>
2023-04-16 22:12:04 +0700

    update react

---
+++ food-order-app/src/App.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
+
+const App = () => {
+	const [cartIsShow, setCartIsShow] = useState(false);
+
+	const showCartHandler = () => {
+		setCartIsShow(true);
+	};
+
+	const hideCartHandler = () => {
+		setCartIsShow(false);
+	};
+
+	return (
+		<CartProvider>
+			{cartIsShow && <Cart onClose={hideCartHandler} />}
+			<Header onShowCart={showCartHandler} />
+			<main>
+				<Meals />
+			</main>
+		</CartProvider>
+	);
+};
+
+export default App;

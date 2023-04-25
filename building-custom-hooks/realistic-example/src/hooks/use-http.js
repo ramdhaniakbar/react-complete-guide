8f2e4d9a69c92d394b655ccef983eb771fe93820
Muhamad Ramdhani Akbar <ramdhaniakbarmuhamad@gmail.com>
2023-04-25 22:26:39 +0700

    update react project

---
+++ building-custom-hooks/realistic-example/src/hooks/use-http.js
@@ -0,0 +1,43 @@
+import { useCallback, useState } from 'react';
+
+const useHttp = (applyData) => {
+	const [isLoading, setIsLoading] = useState(false);
+	const [error, setError] = useState(null);
+
+	const sendRequest = useCallback(
+		async (requestConfig, applyData) => {
+			setIsLoading(true);
+			setError(null);
+
+			try {
+				const res = await fetch(requestConfig.url, {
+					method: requestConfig.method ? requestConfig.method : 'GET',
+					headers: requestConfig.headers ? requestConfig.headers : {},
+					body: requestConfig.body
+						? JSON.stringify(requestConfig.body)
+						: null,
+				});
+
+				if (!res.ok) {
+					throw new Error('Request failed!');
+				}
+
+				const data = await res.json();
+
+				applyData(data);
+			} catch (err) {
+				setError(err.message || 'Something went wrong!');
+			}
+			setIsLoading(false);
+		},
+		[applyData]
+	);
+
+	return {
+		isLoading,
+		error,
+		sendRequest,
+	};
+};
+
+export default useHttp;
